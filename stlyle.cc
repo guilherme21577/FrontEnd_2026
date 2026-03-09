3e1d1c96672bf2288e1778a56fa3e4402551e1cf
guilherme <106540089+guilherme21577@users.noreply.github.com>
2026-03-08 22:47:15 -0300

    Add files via upload

---
+++ stlyle.cc
@@ -0,0 +1,75 @@
+
+*{
+margin: 0px;
+padding: 0px;
+box-shadow: border-box; 
+font-family: "Helvetica", Arial, sans-serif;
+}
+
+#pagina{
+    margin: 20px;
+}
+
+p{
+margin-left: 20px;
+}
+
+h1{
+    text-align: center;
+    justify-content: center;
+    font-size: 40px;
+    margin: 20px;
+    padding: 0px;
+    
+
+}
+
+.sub{
+  text-align: center;
+  justify-content: center;
+     font-size: 25px;
+}
+
+#menu-principal{ 
+    text-align: center;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+
+.primeiro{
+    list-style: none;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    margin: 50px;
+}
+.pio .bug .int .ling .lin .for{
+    display: inline-block;
+    margin: 50px;
+    padding: 10px;
+
+
+}
+
+a{
+margin-right: 50px  ;
+text-decoration: none;
+}
+
+
+section{
+margin: 25px;
+padding: 15px;
+border: 2px solid black;
+}
+
+li{
+list-style: none;
+
+}
+
+
+body{
+    background-color: yellowgreen;
+
+}
